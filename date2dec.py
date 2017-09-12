ded2dfa9ee6c2e093cb3a8dc4d9062bf25d3d22f
simondaout <daouts@geophysik.uni-kiel.de>
2017-09-12 13:59:53 +0200

    add date2dec functions

--- date2dec.py
+++ date2dec.py
@@ -11,3 +11,19 @@ def date2dec(dates):
         # print date,dec,year
         times.append(year + dec)
     return times
+
+def time2dec(dates):
+    dates  = np.atleast_1d(dates)
+    times = []
+    for date in dates:
+        x = datetime.strptime('{}'.format(date),'%Y-%m-%d %H:%M:%S.%f')
+        year = float(x.strftime('%Y'))
+        day = float(x.strftime('%j'))
+        hour = float(x.strftime('%H'))
+        minutes = float(x.strftime('%M'))
+        sec = float(x.strftime('%S.%f'))
+
+        dec = (day*24*60*60 + hour*60*60 + minutes*60 + sec) / (365.1*24*60*60)
+        # print date,dec,year
+        times.append(year + dec)
+    return times
